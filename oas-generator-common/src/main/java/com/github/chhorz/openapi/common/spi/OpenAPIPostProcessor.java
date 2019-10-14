a49dc4f04743766fe63608cb83d207f22c13bc26
chhorz <horz.christian@gmail.com>
2019-10-14 20:28:03 +0200

    Updated documentation

--- oas-generator-common/src/main/java/com/github/chhorz/openapi/common/spi/OpenAPIPostProcessor.java
+++ oas-generator-common/src/main/java/com/github/chhorz/openapi/common/spi/OpenAPIPostProcessor.java
@@ -9,20 +9,22 @@ import com.github.chhorz.openapi.common.domain.OpenAPI;
  */
 public interface OpenAPIPostProcessor {
 
-	/**
-	 * The actual post processing call.
-	 *
-	 * @param openApi the parsed {@link OpenAPI} for which the post processing should be done
-	 */
-	void execute(OpenAPI openApi);
+    /**
+     * The actual post processing call.
+     *
+     * @param openApi the parsed {@link OpenAPI} for which the post processing
+     *                should be done
+     */
+    void execute(OpenAPI openApi);
 
-	/**
-	 * Returns the value for the order in which the post processor should be executed. Possible values are between
-	 * {@code Integer.MIN_VALUE} and {@code Integer.MAX_VALUE}. The processors will be executed starting with the
-	 * lowest value.
-	 *
-	 * @return the order in which the post processor should be executed
-	 */
-	int getPostProcessorOrder();
+    /**
+     * Returns the value for the order in which the post processor should be
+     * executed. Possible values are between {@code Integer.MIN_VALUE} and
+     * {@code Integer.MAX_VALUE}. The processors will be executed starting with
+     * the lowest value.
+     *
+     * @return the order in which the post processor should be executed
+     */
+    int getPostProcessorOrder();
 
 }
