12305537c3aed58126f784e6826d55ef218e6868
Christian Horz <horz.christian@gmail.com>
2022-08-08 11:58:59 +0200

    Added support for `@Size` annotation (#295)

--- oas-generator-common/src/main/java/com/github/chhorz/openapi/common/spi/mapping/ObjectTypeMirrorMapper.java
+++ oas-generator-common/src/main/java/com/github/chhorz/openapi/common/spi/mapping/ObjectTypeMirrorMapper.java
@@ -32,10 +32,9 @@ import javax.lang.model.type.NoType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+
+import jakarta.validation.constraints.*;
+
 import java.lang.annotation.Annotation;
 import java.util.*;
 import java.util.function.Function;
